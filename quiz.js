ece304de31bb6886eab9a4def4ec4b277c641788
Jessica Wynn <Jessicashinjo@gmail.com>
2016-04-19 14:33:34 -0500

    finished product with working text validation

--- quiz.js
+++ quiz.js
@@ -1,6 +1,8 @@
 
 var treeHeight = document.getElementById("inputTextHeight");
 
+
+//makes a tree with character and height provided by user
 function tree() {
   for (var i = 1; i < (treeHeight.value * 2); i+=2) {
     var treeList = [];
@@ -20,20 +22,31 @@ function tree() {
 function pressEnter(e) {
   var code = (e.keyCode ? e.keyCode : e.which);
   if (code === 13) {
-    tree();
+    validateTextField();
   }
-  }
+}
 
-// function userInputValid () {
-//   if (document.getElementById("inputTextHeight").value ||
-//       document.getElementById("inputTextCharacter").value === undefined) {
-//     alert("Please enter a value");
-//   }
-// }
+//Gives alert if text field is not filled in. Otherwise it runs the tree program
+function validateTextField() {
+  var characterText = document.getElementById("inputTextCharacter").value;
+  var heightText = document.getElementById("inputTextHeight").value;
+  if (characterText === null || characterText === "") {
+    alert("Please enter a character");
+  } else if (heightText === null || heightText === "") {
+    alert("Please enter a height");
+  } else {
+    tree ();
+  }
+}
+
 
 //Button event listener to grow tree
-document.getElementById("growTreeButton").addEventListener("click", tree);
+document.getElementById("growTreeButton").addEventListener("click", validateTextField);
 
 //Enter event listener to grow tree
 document.getElementById("inputTextHeight").addEventListener("keypress", pressEnter);
 document.getElementById("inputTextCharacter").addEventListener("keypress", pressEnter);
+
+
+
+
